5f1e6dc2033e41236776914eb23244704a876644
Rajath Rao <raorajath1998@gmail.com>
2024-02-08 13:24:32 +0530

    placeholder for expence-filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React,{useState} from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  let arr=[]
-  for(let i=0;i<props.items.length;i++){
-    arr.push(<ExpenseItem
-      title={props.items[i].title}
-      amount={props.items[i].amount}
-      date={props.items[i].date}
-      location={props.items[i].location}
-    />)
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  let arr = [];
+  for (let i = 0; i < props.items.length; i++) {
+    arr.push(
+      <ExpenseItem
+        title={props.items[i].title}
+        amount={props.items[i].amount}
+        date={props.items[i].date}
+        location={props.items[i].location}
+      />
+    );
   }
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
       {arr}
     </Card>
   );
-}
+};
 
 export default Expenses;
